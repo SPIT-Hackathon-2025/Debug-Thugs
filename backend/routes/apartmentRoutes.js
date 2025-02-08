1cc20c272d3a57ba4e4953c25474056b770e2db2
manishjadhav9 <manishsj289@gmail.com>
2025-02-08 22:05:56 +0530

    contractaddress added in apartmentRoutes

--- backend/routes/apartmentRoutes.js
+++ backend/routes/apartmentRoutes.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 const router = express.Router();
 
 const contractABI = require("../web3/abi/ContractEnforcement.json").abi;
-const contractAddress = "0xYourContractAddress";
+const contractAddress = "0x0Bf8E4990c9F5ea14D4C192C6a31cE734fd692Db";
 
 router.get("/getRent", async (req, res) => {
-  const provider = new ethers.providers.JsonRpcProvider("https://polygon-rpc.com");
+  const provider = new ethers.providers.JsonRpcProvider("https://rpc-amoy.polygon.technology");
   const contract = new ethers.Contract(contractAddress, contractABI, provider);
   const rent = await contract.getRent();
   res.json({ rent: ethers.utils.formatEther(rent) });
