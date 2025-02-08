7352505383e3c6218d368701dbf9a1f0a83138a2
jituMarathe <vinaymarathe8702@gmail.com>
2025-02-08 21:51:43 +0530

    commit server setup completed

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("./config/db");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/apartments", require("./routes/apartmentRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(Server running on port ${PORT}));
